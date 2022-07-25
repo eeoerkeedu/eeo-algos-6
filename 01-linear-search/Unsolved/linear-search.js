687a88aa57505395e2fc264db933fbcb1802e89a
LilTzar <eeoerkeedu@gmail.com>
2022-07-25 16:06:36 -0600

    linear search solved

--- 01-linear-search/Unsolved/linear-search.js
+++ 01-linear-search/Unsolved/linear-search.js
@@ -3,4 +3,20 @@
 // else return -1
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
-var linearSearch = function(arr, target) {};
+var linearSearch = function (arr, target) {
+	// console.log(arr);
+	// console.log(target);
+
+	const indexOfTarget = -1;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === target) {
+			console.log(i);
+			return i;
+		}
+	}
+
+	console.log(indexOfTarget);
+
+	return indexOfTarget;
+};
